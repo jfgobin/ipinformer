18bc3285c311fa0b5ac56cf76423628b3f49a3b6
Jean-Francois Gobin <jeanfgobin@gmail.com>
2014-05-26 21:03:34 -0400

    Addition of config.go

---
+++ config.go
@@ -0,0 +1,14 @@
+/* ***********************************************
+ * config.go
+ *
+ * Functions to read the configuration file
+ *
+ * Revisions
+ * ---------
+ *
+ * 2014 05 26 - v1.0 - Jean Gobin
+ *********************************************** */
+
+package main
+
+
